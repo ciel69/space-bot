aae71412260add73e7bd009336026bd908f27f9a
Бухальцов Алексей <Bukhaltsov@lanit.ru>
2024-11-26 15:31:39 +0300

    fix run prod

--- apps/web/src/components/HelloWorld.vue
+++ apps/web/src/components/HelloWorld.vue
@@ -61,7 +61,7 @@ onBeforeMount(async () => {
     <div class="card">
       <button type="button" @click="count++">count is {{ count }}</button>
       <p>
-        Edit
+        Edit test
         <code>components/HelloWorld.vue</code> to test HMR
       </p>
     </div>
