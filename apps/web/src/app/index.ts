d6360850ff145176ed57ce578d34ec700cf94abf
Бухальцов Алексей <Bukhaltsov@lanit.ru>
2024-11-28 14:08:54 +0300

    отмена выделений

--- apps/web/src/app/index.ts
+++ apps/web/src/app/index.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
-// import { init, backButton, initData } from '@telegram-apps/sdk-vue'
+import { init, backButton, initData, swipeBehavior } from '@telegram-apps/sdk-vue'
 
 import App from './app.vue'
 import { router } from './providers/router-provider/router-provider'
 
-// init()
+init()
 
 export const app = createApp(App).use(router)
-// backButton.mount()
-// initData.restore()
+backButton.mount()
+initData.restore()
+if (swipeBehavior.disableVertical.isAvailable()) {
+    swipeBehavior.disableVertical()
+    swipeBehavior.isVerticalEnabled() // false
+}
