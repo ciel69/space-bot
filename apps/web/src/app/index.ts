82c54e1a26bd517d8b6a6393526a3369481224f3
Бухальцов Алексей <Bukhaltsov@lanit.ru>
2024-11-28 15:13:48 +0300

    добавлена карта

--- apps/web/src/app/index.ts
+++ apps/web/src/app/index.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
-// import { init, backButton, initData, swipeBehavior } from '@telegram-apps/sdk-vue'
+import { init, backButton, initData, swipeBehavior } from '@telegram-apps/sdk-vue'
 
 import App from './app.vue'
 import { router } from './providers/router-provider/router-provider'
 
-// init()
+init()
 
 export const app = createApp(App).use(router)
-// backButton.mount()
-// initData.restore()
+backButton.mount()
+initData.restore()
 
-// if (swipeBehavior.mount.isAvailable()) {
-//     swipeBehavior.mount()
-//     swipeBehavior.disableVertical()
-// }
+if (swipeBehavior.mount.isAvailable()) {
+    swipeBehavior.mount()
+    swipeBehavior.disableVertical()
+}
