6ff7a0bd8008086fa2aa52f66a67561c16393ad1
Бухальцов Алексей <Bukhaltsov@lanit.ru>
2024-12-03 08:51:55 +0300

    test haptic-feedback api

--- apps/web/src/app/index.ts
+++ apps/web/src/app/index.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { init, backButton, initData, swipeBehavior } from '@telegram-apps/sdk-vue'
+import { init, backButton, initData, swipeBehavior, miniApp, themeParams, viewport } from '@telegram-apps/sdk-vue'
 
 import App from './app.vue'
 import { router } from './providers/router-provider/router-provider'
@@ -7,10 +7,22 @@ import { router } from './providers/router-provider/router-provider'
 init()
 
 export const app = createApp(App).use(router)
-backButton.mount()
+if (backButton.isSupported()) {
+    backButton.mount()
+}
+miniApp.mount()
+themeParams.mount()
 initData.restore()
 
 if (swipeBehavior.mount.isAvailable()) {
     swipeBehavior.mount()
     swipeBehavior.disableVertical()
 }
+viewport.mount().then(() => {
+    // Define components-related CSS variables.
+    viewport.bindCssVars()
+    miniApp.bindCssVars()
+    themeParams.bindCssVars()
+}).catch((e: string) => {
+    console.error('Something went wrong mounting the viewport', e)
+})
